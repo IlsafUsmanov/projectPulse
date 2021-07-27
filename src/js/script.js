a72426ab98d9ba5b595424bde1eef1f58e4de5c2
Ilsaf <ilsafusm@gmail.com>
2021-07-27 13:40:31 +0300

    change after create pageup

--- src/js/script.js
+++ src/js/script.js
@@ -92,4 +92,18 @@ $(document).ready(function(){
     })
     return false;
   });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  })
+
+  $("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
 });
