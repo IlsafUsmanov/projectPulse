3981c3fcdb4ed8e80581a5b31b2dafe23e4ae00d
Ilsaf <ilsafusm@gmail.com>
2021-07-27 11:59:03 +0300

    phpmailer

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,19 @@ $(document).ready(function(){
 
 
   $('input[name=phone]').mask("+7 (999) 999-9999");
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn();
+      $('form').trigger('reset');
+    })
+    return false;
+  });
 });
